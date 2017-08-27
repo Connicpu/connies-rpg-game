51dbd55c66c31941df915b5a197066fd6a41cb4b
Connor Hilarides <connorcpu@live.com>
2017-08-27 00:32:05 -0700

    oops, broke the build.rs

--- build.rs
+++ build.rs
@@ -3,9 +3,10 @@ use std::path::PathBuf;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
-    if target.contains("pc-windows") {
-        let mut manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        manifest_dir.push("lib");
+    let mut manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    manifest_dir.push("lib");
+
+    let (mut lib_dir, mut dll_dir) = if target.contains("pc-windows") {
         let mut lib_dir = manifest_dir.clone();
         let mut dll_dir = manifest_dir.clone();
         if target.contains("msvc") {
@@ -16,31 +17,36 @@ fn main() {
             lib_dir.push("gnu-mingw");
             dll_dir.push("gnu-mingw");
         }
-        lib_dir.push("lib");
-        dll_dir.push("dll");
-        if target.contains("x86_64") {
-            lib_dir.push("64");
-            dll_dir.push("64");
-        }
-        else {
-            lib_dir.push("32");
-            dll_dir.push("32");
-        }
-        println!("cargo:rustc-link-search=all={}", lib_dir.display());
-        if let Ok(read_dir) = std::fs::read_dir(dll_dir) {
-            for entry in read_dir {
-                let entry_path = entry.expect("Invalid fs entry").path();
-                let file_name_result = entry_path.file_name();
-                let mut new_file_path = manifest_dir.clone();
-                new_file_path.push("target");
-                new_file_path.push(env::var("PROFILE").unwrap());
 
-                if let Some(file_name) = file_name_result {
-                    let file_name = file_name.to_str().unwrap();
-                    if file_name.ends_with(".dll") {
-                        new_file_path.push(file_name);
-                        std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
-                    }
+        (lib_dir, dll_dir)
+    } else {
+        return;
+    };
+
+    lib_dir.push("lib");
+    dll_dir.push("dll");
+    if target.contains("x86_64") {
+        lib_dir.push("64");
+        dll_dir.push("64");
+    }
+    else {
+        lib_dir.push("32");
+        dll_dir.push("32");
+    }
+    println!("cargo:rustc-link-search=all={}", lib_dir.display());
+    if let Ok(read_dir) = std::fs::read_dir(dll_dir) {
+        for entry in read_dir {
+            let entry_path = entry.expect("Invalid fs entry").path();
+            let file_name_result = entry_path.file_name();
+            let mut new_file_path = manifest_dir.clone();
+            new_file_path.push("target");
+            new_file_path.push(env::var("PROFILE").unwrap());
+
+            if let Some(file_name) = file_name_result {
+                let file_name = file_name.to_str().unwrap();
+                if file_name.ends_with(".dll") {
+                    new_file_path.push(file_name);
+                    std::fs::copy(&entry_path, new_file_path.as_path()).expect("Can't copy from DLL dir");
                 }
             }
         }
