48562f796a08b0003975fa89d7ed6c91a56c10c6
Liam <liam.tab@gmail.com>
2017-09-01 01:13:16 -0700

    added draw call counter

--- src/graphics/systems/end_frame.rs
+++ src/graphics/systems/end_frame.rs
@@ -13,6 +13,10 @@ fn process(_: &mut EndFrame, data: &mut DataHelper) {
         .current_frame
         .take()
         .map(|f| f.finish().unwrap());
+    
+    println!("draw count: {}", data.services.graphics.draw_count);
+    
+    data.services.graphics.draw_count = 0;
 
     let min_frametime = 1_000_000_000 / CONFIG.graphics.max_fps as u64;
     while data.services.timer.immediate_frametime_ns() < min_frametime {
